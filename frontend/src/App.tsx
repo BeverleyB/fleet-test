ff07e3cdc86ca93cb1234152e98c6db7ace29780
BeverleyB <beverley.beranger@gmail.com>
2024-04-09 18:17:50 +0200

    Add router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import MoviesList from './pages/MoviesList/MoviesList';
+import MovieDetail from './pages/MovieDetail/MovieDetail';
+import SearchBar from './components/SearchBar/SearchBar';
+
 import './App.scss';
 
 function App() {
+
   return (
     <div className="App">
-      Hello World
+      <Router>
+        <SearchBar />
+        <Routes>
+          <Route path="/" element={<MoviesList />} />
+          <Route path="/movie/:id" element={<MovieDetail />} />
+        </Routes>
+      </Router>
     </div>
   );
 }

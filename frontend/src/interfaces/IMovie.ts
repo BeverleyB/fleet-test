89665c06e3cd9cc7f9c08d7d65438d0adf2686b0
BeverleyB <beverley.beranger@gmail.com>
2024-04-10 13:10:54 +0200

    Update attribut interfaces

--- frontend/src/interfaces/IMovie.ts
+++ frontend/src/interfaces/IMovie.ts
@@ -2,6 +2,7 @@ export interface IMovie {
     adult: boolean,
     backdrop_path: string
     genre_ids: [],
+    genres: [],
     id: number,
     original_language: string,
     original_title: string,
@@ -9,8 +10,30 @@ export interface IMovie {
     popularity: number
     poster_path: string,
     release_date: string
-    title: string
+    title: string,
+    tagline: string,
     video: boolean,
     vote_average: number,
-    vote_count: number
+    vote_count: number,
+    production_companies: []
+}
+
+export const DefaultMovie: IMovie = {
+    adult:false,
+    backdrop_path:'',
+    genre_ids:[],
+    genres: [],
+    id:1,
+    original_language:'en',
+    original_title:'',
+    overview:'',
+    popularity:1,
+    poster_path:'https://image.tmdb.org/t/p/original/',
+    release_date:'',
+    title:'',
+    tagline: '',
+    video:true,
+    vote_average:1,
+    vote_count:1,
+    production_companies: []
 }
